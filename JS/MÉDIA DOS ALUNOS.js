a740df555e9621bf3dca89295183ce7218af6e76
joão <joao.a.guedes@aluno.senai.br>
2026-06-11 21:51:36 -0300

    resto

--- JS/MÉDIA DOS ALUNOS.js
+++ JS/MÉDIA DOS ALUNOS.js
@@ -11,11 +11,14 @@ formDados.addEventListener('submit', (evt) => {
     var num1 = parseFloat(form_num.get('num1'))
     var num2 = parseFloat(form_num.get('num2'))
     var num3 = parseFloat(form_num.get('num3'))
+    var num4 = parseFloat(form_num.get('num4'))
+
+
 
 
     var media = (parseFloat(num2 + num3 + num4)) / parseFloat(3)
 
 
 
-    divResultado.innerHTML = `tem como média ${media.toFixed(2).replace('.',',')} ${media >= 6 , "aprovado" && "reprovado"}`
+    divResultado.innerHTML = `tem como média ${media.toFixed(2).replace('.',',')} ${media >= 6 ? "aprovado" : "reprovado"}`
 })
